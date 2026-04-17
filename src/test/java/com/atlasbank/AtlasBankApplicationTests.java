e15b99f09c58f28ce97f718afa2dd861b871ee52
Gabriel Raiol Rodrigues <gabrielraiolr@gmail.com>
2026-04-17 01:02:33 -0300

    chore: scaffold atlas-bank with hexagonal module structure

---
+++ src/test/java/com/atlasbank/AtlasBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlasbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
